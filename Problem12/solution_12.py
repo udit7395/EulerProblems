46a2d0155f7b9613f3f137280613f36d9a2cd949
udit7395 <udit7395@gmail.com>
2019-03-26 01:57:26 +0530

    Remove comments && Optimized Problem12 abit

--- Problem12/solution_12.py
+++ Problem12/solution_12.py
@@ -20,17 +20,12 @@ aggregate = int(sum_upto_n_numbers(limit))
 
 while(len(divisors) * 2 < 501):
     aggregate = int(sum_upto_n_numbers(limit))
-    print("Aggregate : %d" % aggregate)    
-    print("Limit : %d" % limit)
     divisors.clear()
     divisors.append(1)
-    for number in range(2, int(math.sqrt(aggregate) + 1)):
+    for number in range(2, int(math.sqrt(aggregate)) + 1):
         if(is_divisible_by_x(aggregate, number)):
             divisors.append(number)  
     divisors.append(aggregate)
     limit = limit + 1
-    print("Divisor  : {}".format(divisors))
-    print("Divisor len : %d" % len(divisors))
-    print("")
 
 timer("The value of the first triangle number to have over five hundred divisors is {}".format(aggregate))
